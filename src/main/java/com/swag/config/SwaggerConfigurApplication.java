c77e518ff9fcfa305e3660b08ef84c0be78dd70f
Ranjeet <ranjeetkumar.it.20183037@gmail.com>
2022-12-29 17:32:30 +0530

    initial commit

---
+++ src/main/java/com/swag/config/SwaggerConfigurApplication.java
@@ -0,0 +1,13 @@
+package com.swag.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerConfigurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerConfigurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swag/config/SwaggerConfigurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swag.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerConfigurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
